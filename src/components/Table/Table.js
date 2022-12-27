7d52ee83d7b5b7b6af0993dd9a87b68d6bb51a89
Maksumoff <muxammadamirulloxon@gmail.com>
2022-12-27 16:42:10 +0500

    crud-table-ready

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -9,6 +9,7 @@ export default class Table extends Component {
       name: "",
       status: "",
       search: "id",
+      active: null,
     };
   }
   render() {
@@ -31,7 +32,40 @@ export default class Table extends Component {
       this.setState({ search: e.target.value });
     };
 
-    const addHandler = (e) => {};
+    const addHandler = () => {
+      let newUser = {
+        id: Date.now(),
+        name: this.state.name,
+        status: this.state.status,
+      };
+      this.setState({
+        data: [...this.state.data, newUser],
+        name: "",
+        status: "",
+      });
+    };
+
+    const deleteHandler = (id) => {
+      console.log(id);
+      let res = this.state.data.filter((value) => value.id !== id);
+      this.setState({ data: res });
+    };
+    const editHandler = ({ id, name, status }, isSave) => {
+      if (isSave) {
+        let res = this.state.data.map((value) =>
+          value.id === this.state.active.id
+            ? { ...value, name: this.state.name, status: this.state.status }
+            : value
+        );
+        this.setState({ active: null, data: res });
+      } else {
+        this.setState({
+          name: name,
+          status: status,
+          active: { id, name, status },
+        });
+      }
+    };
 
     const { name, status, data } = this.state;
     return (
@@ -40,12 +74,14 @@ export default class Table extends Component {
           <h1>Name: {name} </h1>
           <h1>status: {status} </h1>
           <input
+            value={name}
             name="name"
             onChange={changeInput}
             type="text"
             placeholder="name"
           />
           <input
+            value={status}
             name="status"
             onChange={changeInput}
             type="text"
@@ -81,17 +117,56 @@ export default class Table extends Component {
               </tr>
             </thead>
             <tbody>
-              {data.map(({ id, name, status }) => {
-                return (
-                  <tr key={id}>
-                    <td>{id} </td>
-                    <td>{name} </td>
-                    <td>{status} </td>
-                    <td>delete </td>
-                    <td>edit</td>
-                  </tr>
-                );
-              })}
+              {data.length ? (
+                data.map(({ id, name, status }) => {
+                  return (
+                    <tr key={id}>
+                      <td>{id} </td>
+                      <td>
+                        {this.state.active?.id === id ? (
+                          <input
+                            name="name"
+                            onChange={changeInput}
+                            value={this.state.name}
+                            type="text"
+                          />
+                        ) : (
+                          name
+                        )}
+                      </td>
+                      <td>
+                        {this.state.active?.id === id ? (
+                          <input
+                            name="status"
+                            onChange={changeInput}
+                            value={this.state.status}
+                            type="text"
+                          />
+                        ) : (
+                          status
+                        )}{" "}
+                      </td>
+                      <td onClick={() => deleteHandler(id)}>delete </td>
+                      <td
+                        onClick={() =>
+                          editHandler(
+                            { id, name, status },
+                            this.state.active?.id === id
+                          )
+                        }
+                      >
+                        {this.state.active?.id === id ? "save" : "edit"}
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr>
+                  <th colSpan={5}>
+                    <h1>no data</h1>
+                  </th>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
