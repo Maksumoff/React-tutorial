a043da0e9b0509839d720ce22c13465dbeb691a1
Maksumoff <muxammadamirulloxon@gmail.com>
2023-03-29 17:29:45 +0500

    Delete

--- src/Components/Get Data/getData.jsx
+++ src/Components/Get Data/getData.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 const GetData = () => {
   const [houseList, setHouseList] = useState([]);
+
   const getInfo = () => {
     return fetch("http://158.51.99.245:8081/api/v1/houses/list", {
       headers: {
@@ -10,22 +11,48 @@ const GetData = () => {
       .then((res) => res.json())
       .then((res) => {
         setHouseList(res?.data);
-        console.log(res);
+        // console.log(res);
       });
   };
 
+  //sort 'houseList' in an ascending order
   const sortId = houseList.sort((a, b) => a.id - b.id);
 
+  const onDelete = (id) => {
+    return fetch(`http://158.51.99.245:8081/api/v1/houses/${id}`, {
+      method: "Delete",
+      headers: {
+        Auhorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res?.success) {
+          getInfo();
+        }
+        // res?.success && getInfo()
+      });
+  };
+
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "5px",
+      }}
+    >
       <button onClick={getInfo}>Get Data</button>
       {sortId.map(({ id, name, description }) => (
-        <div key={id}>
-          <h3>
+        <div key={id} style={{ display: "flex" }}>
+          <h3 style={{ marginRight: "5px" }}>
             Name: {id}
             {"."} {name}
+            {"."}
           </h3>
-          <h4>description: {description}</h4>
+
+          <h3 style={{ marginRight: "5px" }}>Description: {description}</h3>
+          <button onClick={() => onDelete(id)}>delete</button>
         </div>
       ))}
     </div>
